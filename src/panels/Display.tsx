4e90121ca8efa16b5bc70c8e2665b03ca55175e4
nickbaldwin <nicholas.baldwin@monster.com>
2021-06-29 10:09:55 +1000

    scroll table and sample data

--- src/panels/Display.tsx
+++ src/panels/Display.tsx
@@ -9,12 +9,18 @@ import { Grid, GridColumn } from '@progress/kendo-react-grid';
 import { loadSettings, saveSettings } from "../helpers/state";
 
 let products = [
-    {"ProductID": 1, "ProductName": "Tea", "CategoryID": 1, "QuantityPerUnit": "10 boxes x 20 bags", "UnitPrice": 18.0, "UnitsInStock": 39, "Discontinued": false },
-    {"ProductID": 7, "ProductName": "Dried Pears", "CategoryID": 7, "QuantityPerUnit": "12 - 1 lb pkgs.", "UnitPrice": 30.0, "UnitsInStock": 15, "Discontinued": false },
-    {"ProductID": 8, "ProductName": "Cranberry Sauce", "CategoryID": 2, "QuantityPerUnit": "12 - 12 oz jars", "UnitPrice": 40.0, "UnitsInStock": 6, "Discontinued": false },
-    {"ProductID": 9, "ProductName": "Mishi Kobe Niku", "CategoryID": 6, "QuantityPerUnit": "18 - 500 g pkgs.", "UnitPrice": 97.0, "UnitsInStock": 29, "Discontinued": true },
-    {"ProductID": 14, "ProductName": "Tofu", "CategoryID": 7, "QuantityPerUnit": "40 - 100 g pkgs.", "UnitPrice": 23.25, "UnitsInStock": 35, "Discontinued": false },
-    {"ProductID": 17, "ProductName": "Alice Mutton", "CategoryID": 6, "QuantityPerUnit": "20 - 1 kg tins", "UnitPrice": 39.0, "UnitsInStock": 0, "Discontinued": false},
+    {"position": 1, "company": "Suzuki", "title": "Driver", "location": "Colorado Springs, CO 80910", "ingestion": "JPW", "type": "2", "adProvider": "Adtech", "mesco": "4300746001001" },
+    {"position": 2, "company": "Maaas", "title": "Receptionist", "location": "Colorado Springs, CO 80910", "ingestion": "NOW", "type": "2", "adProvider": "Adtech", "mesco": "4300746001001" },
+    {"position": 3, "company": "Apple", "title": "Software engineer", "location": "Colorado Springs, CO 80910", "ingestion": "NOW", "type": "2", "adProvider": "Adtech", "mesco": "4300746001001" },
+    {"position": 4, "company": "IBM", "title": "Software development manager", "location": "Colorado Springs, CO 80910", "ingestion": "NOW", "type": "2", "adProvider": "Adtech", "mesco": "4300746001001" },
+    {"position": 5, "company": "Randstad", "title": "Sales manager", "location": "Colorado Springs, CO 80910", "ingestion": "JPW", "type": "2", "adProvider": "Adtech", "mesco": "4300746001001" },
+    {"position": 6, "company": "Monster", "title": "Product owner", "location": "remote", "ingestion": "NOW", "type": "3", "adProvider": "Adtech", "mesco": "4300746001001"},
+    {"position": 1, "company": "Suzuki", "title": "Driver", "location": "Colorado Springs, CO 80910", "ingestion": "JPW", "type": "2", "adProvider": "Adtech", "mesco": "4300746001001" },
+    {"position": 2, "company": "Maaas", "title": "Receptionist", "location": "Colorado Springs, CO 80910", "ingestion": "NOW", "type": "2", "adProvider": "Adtech", "mesco": "4300746001001" },
+    {"position": 3, "company": "Apple", "title": "Software engineer", "location": "Colorado Springs, CO 80910", "ingestion": "NOW", "type": "2", "adProvider": "Adtech", "mesco": "4300746001001" },
+    {"position": 4, "company": "IBM", "title": "Software development manager", "location": "Colorado Springs, CO 80910", "ingestion": "NOW", "type": "2", "adProvider": "Adtech", "mesco": "4300746001001" },
+    {"position": 5, "company": "Randstad", "title": "Sales manager", "location": "Colorado Springs, CO 80910", "ingestion": "JPW", "type": "2", "adProvider": "Adtech", "mesco": "4300746001001" },
+    {"position": 6, "company": "Monster", "title": "Product owner", "location": "remote", "ingestion": "NOW", "type": "3", "adProvider": "Adtech", "mesco": "4300746001001"},
 ];
 
 export const Display = () => {
@@ -23,10 +29,14 @@ export const Display = () => {
             <p>hello</p>
             <Grid
                 data={products}>
-                <GridColumn field="ProductName" />
-                <GridColumn field="UnitPrice" />
-                <GridColumn field="UnitsInStock" />
-                <GridColumn field="Discontinued" />
+                <GridColumn field="position" />
+                <GridColumn field="company" />
+                <GridColumn field="title" />
+                <GridColumn field="location" />
+                <GridColumn field="ingestion" />
+                <GridColumn field="type" />
+                <GridColumn field="adProvider" />
+                <GridColumn field="mesco" />
             </Grid>
         </div>
     );
