75305b299bc89f33c756a8ec178b9a523376d814
nickbaldwin-monster <nicholas.baldwin@monster.com>
2021-11-12 19:42:30 +1100

    Updated authentication

--- src/components/AuthButtons.tsx
+++ src/components/AuthButtons.tsx
@@ -2,19 +2,13 @@ import * as React from "react";
 
 import { Button } from "@progress/kendo-react-buttons";
 
-import { sendMessageToBackgroundAndPopup, sendMessageToContent } from "../helpers/messaging";
+import { sendMessageToBackgroundAndPopup } from "../helpers/messaging";
 
 
 
 // @ts-ignore
 export const AuthButtons = ({ auth, name} ) => {
 
-    const handleCheck = () => {
-        sendMessageToBackgroundAndPopup({
-            type: "LOGIN_STATUS_REQUEST",
-            source: "loginPanel"
-        });
-    };
     const handleLogin = () => {
         sendMessageToBackgroundAndPopup({
             type: "LOGIN_REQUEST",
@@ -28,20 +22,6 @@ export const AuthButtons = ({ auth, name} ) => {
         });
     };
 
-    if (false) {
-        return (
-            <div style={{ padding: "5px 0" }}>
-                {auth && <span>Authenticated: {name}</span>}
-                {!auth && <span>You need to login </span>}
-                <Button onClick={auth? handleLogout: handleLogin}>Logout</Button>
-
-                &nbsp;&nbsp;
-                <Button look="flat" onClick={handleCheck}>Check</Button>&nbsp;
-                <Button look="flat" onClick={handleLogin}>Login</Button>&nbsp;
-                <Button look="flat" onClick={handleLogout}>Logout</Button>
-            </div>
-        );
-    }
     return (
         <div style={{ padding: "5px 0" }}>
             {auth ?
--- src/panels/LoginPanel.tsx
+++ src/panels/LoginPanel.tsx
@@ -10,42 +10,18 @@ import { Panel } from "./Panel";
 // @ts-ignore
 export const LoginPanel = () => {
 
-    const handleCheck = () => {
-        sendMessageToBackgroundAndPopup({
-            type: "LOGIN_STATUS_REQUEST",
-            source: "loginPanel"
-        });
-    };
     const handleLogin = () => {
         sendMessageToBackgroundAndPopup({
             type: "LOGIN_REQUEST",
             source: "loginPanel"
         });
     };
-    const handleLogout = () => {
-        sendMessageToBackgroundAndPopup({
-            type: "LOGOUT_REQUEST",
-            source: "loginPanel"
-        });
-    };
-
-    let message = "You may need to login.";
-
-    if (false) {
-        return (
-            <>
-                <Button onClick={handleCheck}>Check</Button>
-                <Button onClick={handleLogin}>Login</Button>
-                <Button onClick={handleLogout}>Logout</Button>
-            </>
-        );
-    }
 
     return (
         <Panel>
             <div className="scrollInPanel">
                 <h4>Login</h4>
-                <p>{message}</p>
+                <p>You may need to login.</p>
                 <br />
                 <Button onClick={handleLogin}>Login</Button>
             </div>
